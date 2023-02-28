6eb98faa360f11bd63b8a9da0ab8bae489254ab1
Sebastian Bürgel <s.buergel@gmail.com>
2023-02-28 00:30:07 -0500

    timeouts and catching errors

--- RPCh-mon.py
+++ RPCh-mon.py
@@ -2,12 +2,16 @@ from web3 import Web3
 import time
 
 endpoint = "http://localhost:8080/?exit-provider=https://primary.gnosis-chain.rpc.hoprtech.net"
-web3 = Web3(Web3.HTTPProvider(endpoint))
+timeout = 5
+web3 = Web3(Web3.HTTPProvider(endpoint, request_kwargs={'timeout': timeout}))
 
 while True:
     start = time.time()
-    blockNo = web3.eth.get_block_number()
+    try:
+        blockNo = web3.eth.get_block_number()
+    except:
+        blockNo = 0
     end = time.time()
     latency = end - start
     print(f"block number: {blockNo}, took {latency:.2f}s")
-    time.sleep(1)
+    time.sleep(2)
