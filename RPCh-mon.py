d3102b9643506f4e5df003c244a05763ae92ae75
Sebastian Bürgel <s.buergel@gmail.com>
2023-02-28 10:16:29 -0500

    print aggregate stats

--- RPCh-mon.py
+++ RPCh-mon.py
@@ -4,6 +4,7 @@ import time
 endpoint = "http://localhost:8080/?exit-provider=https://primary.gnosis-chain.rpc.hoprtech.net"
 timeout = 5
 web3 = Web3(Web3.HTTPProvider(endpoint, request_kwargs={'timeout': timeout}))
+results = []
 
 while True:
     start = time.time()
@@ -14,4 +15,16 @@ while True:
     end = time.time()
     latency = end - start
     print(f"block number: {blockNo}, took {latency:.2f}s")
+    result = {
+            "time": start,
+            "latency": latency,
+            "blockNumber": blockNo
+            }
+    results.append(result)
+    latencies = [r["latency"] for r in results if r["blockNumber"] > 0]
+    numTimeouts = len([r["latency"] for r in results if r["blockNumber"] == 0])
+    numOverThreshold = len([r["latency"] for r in results if r["blockNumber"] > 0 and r["latency"] > timeout])
+    print(f"number of requests that didn't time out: {len(latencies)}")
+    print(f"number of timeouts: {numTimeouts}")
+    print(f"number of requests that didn't time out book took over {timeout}s: {numOverThreshold}")
     time.sleep(2)
